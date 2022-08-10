d05a6e4267e82a381790abc344c3db165680841e
Valen <valenpschool@gmail.com>
2022-08-10 20:45:26 -0300

    feat: featured post

--- src/components/featured-post/index.jsx
+++ src/components/featured-post/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './styles.css';
-import {Link} from 'react-router-dom';
+import AuthorBadge from "../author-badge";
 export default function FeaturedPost(){
     return (
         <div className="featured-post-container">
@@ -8,11 +8,11 @@ export default function FeaturedPost(){
         <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fget.wallhere.com%2Fphoto%2FLinux-Windows-10-1920873.jpg&f=1&nofb=1" alt="" />
         </div>
         <div>
-            <p>July 24, 2020</p>
-            <h2>Título Teste</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quos delectus saepe expedita et voluptatibus, perspiciatis iusto inventore quidem consequuntur </p>
+            <p className="featured-post-date">July 24, 2020</p>
+            <h2 className="featured-post-title">Título Teste</h2>
+            <p className="featured-post-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quos delectus saepe expedita et voluptatibus, perspiciatis iusto inventore quidem consequuntur </p>
         </div>
-
+            <AuthorBadge/>
        </div>
     );
 }
