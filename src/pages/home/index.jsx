9c384df964854e54e52e89cafbc20e33b659a079
Valen <valenpschool@gmail.com>
2022-08-11 00:06:01 -0300

    feat: home page with generated posts

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,25 @@
+import {useState, useEffect} from 'react';
 import FeaturedPost from "../../components/featured-post"
+import PostList from "../../components/post-list"
 
 export default function Home(){
+    const [posts, setPosts] = useState();
+    async function getPosts(){
+        const response = await fetch('https://cms-blog-tutorial.herokuapp.com/api/posts?populate=*');
+        console.log(response)
+        const data = await response.json();
+        console.log(data);
+        setPosts(data.data)
+    }
+    useEffect(() => {
+        getPosts();
+    }, [])
     return(
-        <FeaturedPost />
+        <div>
+            {
+                posts && posts.length > 0 && <FeaturedPost post={posts[0].attributes}/>
+            }
+            <PostList posts={posts} />
+        </div>
     ); 
 }
