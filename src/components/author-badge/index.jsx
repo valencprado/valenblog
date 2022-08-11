2d6b3dfa8a4c0947d8ca30bbed4f8d9e2b53dc77
Valen <valenpschool@gmail.com>
2022-08-11 00:03:31 -0300

    fix: atuhor badge + styles

--- src/components/author-badge/index.jsx
+++ src/components/author-badge/index.jsx
@@ -1,9 +1,9 @@
 import './styles.css';
-
+import profileImage from '../../assets/img/profile.png'
 export default function AuthorBadge(){
     return(
         <div className='author-badge-container'>
-            <img src={profileImage} />
+            <img src={profileImage} className='author-badge-image'/>
             <div>
                 <h3>Valentina Prado</h3>
                 <p>Dev Full-Stack</p>
