fca2aa65070b6835b638e55c09a2ccbd4499a014
Valen <valenpschool@gmail.com>
2022-08-11 00:04:15 -0300

    fix: remove static attributes

--- src/components/featured-post/index.jsx
+++ src/components/featured-post/index.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import './styles.css';
 import AuthorBadge from "../author-badge";
-export default function FeaturedPost(){
+import { Link } from "react-router-dom";
+import Post from "../post";
+export default function FeaturedPost({post}){
     return (
         <div className="featured-post-container">
         <div>
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fget.wallhere.com%2Fphoto%2FLinux-Windows-10-1920873.jpg&f=1&nofb=1" alt="" />
+        <img className="featured-post-image" src={post?.hero?.data?.attributes?.formats?.medium?.url} alt="" />
         </div>
         <div>
-            <p className="featured-post-date">July 24, 2020</p>
-            <h2 className="featured-post-title">Título Teste</h2>
-            <p className="featured-post-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quos delectus saepe expedita et voluptatibus, perspiciatis iusto inventore quidem consequuntur </p>
+            <p className="featured-post-date">{post.date}</p>
+            <Link to='#'>
+            <h2 className="featured-post-title">{post.title}</h2>
+            </Link>
+            <p className="featured-post-description">{post.description}</p>
+        <AuthorBadge/>
         </div>
-            <AuthorBadge/>
        </div>
     );
 }
