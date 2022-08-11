076fc15805e46983a9dc664228c962e0a5cd44bf
Valen <valenpschool@gmail.com>
2022-08-11 00:05:13 -0300

    feat: post component

---
+++ src/components/post/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './styles.css';
+import { Link } from "react-router-dom";
+
+export default function Post({post}){
+    return (
+        <div className="post-container">
+        <div>
+        <img className="post-image" src={post?.hero?.data?.attributes?.formats?.medium?.url} alt="" />
+        </div>
+        <div>
+            <p className="post-date">{post.date}</p>
+            <Link to='#'>
+            <h3 className="post-title">{post.title}</h3>
+            </Link>
+            <p className="post-description">{post.description}</p>
+        </div>
+       </div>
+    );
+}
