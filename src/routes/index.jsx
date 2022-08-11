309a7d06eb1d02365e7b2d7e175132f525c40fd2
Valen <valenpschool@gmail.com>
2022-08-11 00:06:12 -0300

    fix: remove unnecessary route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,13 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from '../pages/home';
 import NavBar from '../components/navbar';
-
+import PostList from '../components/post-list';
 export default function AppRoutes(){
     return(
         <BrowserRouter>
         <NavBar/>
         <Routes>
             <Route path='/' element={<Home />}/>
-            <Route path='/post' element={<h1>Post</h1>}/>
         </Routes>
         </BrowserRouter>
     );
